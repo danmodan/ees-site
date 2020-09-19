cfbc6bbcdd79d59732366c3eff7760c808064684
Danilo F. N. Moreira <danilofnmoreira@gmail.com>
2020-09-19 18:50:39 -0300

    added health check

--- src/main/java/br/com/ees/site/client/sendgrid/SendgridClient.java
+++ src/main/java/br/com/ees/site/client/sendgrid/SendgridClient.java
@@ -1,10 +1,12 @@
 package br.com.ees.site.client.sendgrid;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.ees.site.client.config.SendgridFeignClientConfig;
+import br.com.ees.site.client.sendgrid.model.User;
 import br.com.ees.site.client.sendgrid.request.SendInboundMarketingRequest;
 
 @FeignClient(name = "sendgridClient",  
@@ -14,5 +16,8 @@ public interface SendgridClient {
 
     @PostMapping(path = { "/v3/mail/send" })
     void sendEmail(@RequestBody SendInboundMarketingRequest request);
+
+    @GetMapping(path = { "/v3/user/email" })
+    User getUserEmail();
 
 }
--- src/main/java/br/com/ees/site/controller/v1/HealthController.java
+++ src/main/java/br/com/ees/site/controller/v1/HealthController.java
@@ -1,14 +1,38 @@
 package br.com.ees.site.controller.v1;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ees.site.client.sendgrid.SendgridClient;
+import br.com.ees.site.client.sendgrid.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
-public class HealthController {
+@Slf4j
+@RequiredArgsConstructor
+public class HealthController implements InitializingBean {
+
+    private final SendgridClient sendgridClient;
 
     @GetMapping(value = { "/health" })
-    public String health() {
-        return "k";
+    @ResponseStatus(code = HttpStatus.OK)
+    public final void health() {
+        try {
+            User user = sendgridClient.getUserEmail();
+            log.info(user.toString());
+        } catch (Exception e) {
+            log.error("health check error", e);
+            throw e;
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        health();
     }
 
 }

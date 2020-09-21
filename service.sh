be433c765331c3d82e21d907233ba7655b391872
Danilo F. N. Moreira <danilofnmoreira@gmail.com>
2020-09-20 22:31:39 -0300

    dockerized project

---
+++ service.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+BASEDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+run_maven() {
+
+    docker run --rm \
+               -w /build \
+               -v $BASEDIR:/build \
+               -v ~/.m2:/root/.m2 \
+               maven:3-adoptopenjdk-11-openj9 $@
+}
+
+run_node() {
+
+    docker run -it \
+               --rm \
+               -v $BASEDIR:/usr/src/app \
+               -w /usr/src/app \
+               node $@
+}
+
+case $1 in
+    compile-sass)
+        run_node npx sass --watch theme/scss/main.scss:public/css/main.css --style compressed
+        ;;
+    up-function)
+        docker-compose up -d function.ees.dev
+        ;;
+    log-function)
+        docker logs -f --tail 1000 function.ees.dev
+        ;;
+    down)
+        docker-compose down
+        ;;
+    build)
+        run_maven mvn clean package -f function/pom.xml -DskipTests --batch-mode
+        ;;
+    deploy-function)
+        gcloud functions deploy ees-contact-function --entry-point br.com.ees.function.ContactFormFunction \
+                                                     --runtime java11 \
+                                                     --trigger-http \
+                                                     --memory 512MB \
+                                                     --source function \
+                                                     --allow-unauthenticated
+        ;;
+    *)
+        echo -e "Invalid option"
+        ;;
+esac
